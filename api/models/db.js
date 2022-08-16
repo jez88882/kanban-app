3b107510aa4af2f1e40d80bfd29c410ba9fe1c6f
Jia En <jiaen.zhang98@gmail.com>
2022-08-16 11:22:14 +0800

    fixed error message

--- api/models/db.js
+++ api/models/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize('database', 'username', 'password', {
   dialect: 'mysql',
   dialectOptions: {
     // Your mysql2 options here
-    host     : "host.docker.internal",
+    host     : process.env.DB_HOST,
     user     : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_DATABASE
